9e177c909c8c34ebc37c7070966c2eb06e7db3a0
qupeng <qupeng@pingcap.com>
2018-07-27 17:09:02 +0800

    fix.

--- src/rw.rs
+++ src/rw.rs
@@ -1,18 +1,27 @@
 extern crate rand;
 use std::fs::OpenOptions;
+use std::io::prelude::*;
+use std::io::SeekFrom;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
-    let mut data = Vec::with_capacity::<u8>(1 << 20);
-    (0..1 << 20).for_each(data.push('a'));
+    let mut data = Vec::with_capacity(1 << 20);
+    (0..1 << 20).for_each(|_| data.push(b'a'));
 
-    let mut f = OpenOptions::new().write(true).read(true).create(true).open("a/x");
+    let mut f = OpenOptions::new()
+        .write(true)
+        .read(true)
+        .create(true)
+        .open("a/x")
+        .unwrap();
     f.write_all(&data).unwrap();
 
+    let mut buf = vec![0; 4096];
     loop {
-        let offset: usize = rand::random() % (1 << 20);
-        f.seek(SeekFrom)
+        let offset = rand::random::<u64>() % (1 << 20);
+        f.seek(SeekFrom::Start(offset)).unwrap();
+        f.read(&mut buf).unwrap();
+        thread::sleep(Duration::from_millis(100));
     }
-
-
-    println!("hello world");
 }
